8d86b3861ad216d960de3989d3e192037d19aeac
Deepak Bagada <dbagada910@gmail.com>
2025-08-30 06:57:16 +0000

    Download the React DevTools for a better development experience: https:/

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,19 +3,18 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
 export function createSupabaseServerClient(serverComponent = false) {
-  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value;
+          return cookies().get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           if (serverComponent) return;
           try {
-            cookieStore.set({ name, value, ...options });
+            cookies().set({ name, value, ...options });
           } catch (error) {
             // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -25,7 +24,7 @@ export function createSupabaseServerClient(serverComponent = false) {
         remove(name: string, options: CookieOptions) {
           if (serverComponent) return;
           try {
-            cookieStore.set({ name, value: '', ...options });
+            cookies().set({ name, value: '', ...options });
           } catch (error) {
             // The `delete` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
