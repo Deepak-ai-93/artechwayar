838c709537aaaff4dac157f99998de574e209965
Deepak Bagada <dbagada910@gmail.com>
2025-10-02 16:44:13 +0000

    made by Saasnext "https://saasnext.in/" (_for element div_)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="border-t py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          &copy; {new Date().getFullYear()} Artechway. All rights reserved.
+          Made by <a href="https://saasnext.in/" target="_blank" rel="noopener noreferrer" className="font-medium underline underline-offset-4">Saasnext</a>
         </p>
       </div>
     </footer>
